16c82c297ac526592868a7b61c006dc7a08bc906
Marta Mullor <marta.mullor.polo@gmail.com>
2021-02-04 20:49:46 +0100

    Feature: under construction page done

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../icons/Logo";
+
+const App = () => {
+  return (
+    <div className="under-construction">
+      <h1 className="under-construction-title">Febrero del 2021.</h1>
+      <Logo width={100} />
+    </div>
+  );
+};
+
+export default App;
