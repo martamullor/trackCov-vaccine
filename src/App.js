43d918445532c2649ea4e61872282635e02761a3
Marta Mullor <marta.mullor.polo@gmail.com>
2021-02-04 21:57:27 +0100

    Added sass to the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UnderConstruction from "./pages/UnderConstruction";
-import "./App.css";
+import "./App.scss";
 
 const App = () => {
   return <UnderConstruction />;
