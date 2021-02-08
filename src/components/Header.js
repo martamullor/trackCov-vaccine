4184e901ff44c0a19ca76ab484125f6aa7c4632b
Marta Mullor <marta.mullor.polo@gmail.com>
2021-02-08 18:10:34 +0100

    back to javascript project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../icons/Logo";
+
+const Header = () => {
+  return (
+    <div className="under-construction">
+      <Logo width={100} />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import "./styles/home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
