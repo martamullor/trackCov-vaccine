9dad1ecdc08dd355e0a0eed13a1286022840eb0b
Marta Mullor <marta.mullor.polo@gmail.com>
2021-05-23 09:06:18 +0200

    Change title

--- src/pages/UnderConstruction/UnderConstruction.js
+++ src/pages/UnderConstruction/UnderConstruction.js
@@ -5,7 +5,7 @@ import "./UnderConstruction.scss";
 const UnderConstruction = () => {
   return (
     <div className="under-construction">
-      <h1 className="under-construction-title">Primavera del 2021</h1>
+      <h1 className="under-construction-title">Disponible en 2021</h1>
       <Logo width={100} />
     </div>
   );
